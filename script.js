8c36a6672e6ab993e7e6682b0279c6b80e10329a
ccsjib <jsyed@cannonschool.org>
2023-05-05 13:48:08 -0400

    changed js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 <script>
-  setTimeout(function() {
-    // show the pop-up message
-    document.getElementById("popup").style.display = "block";
-
-    // animate the transition between old and new websites
-    document.getElementById("old-website").style.transform = "translateX(-100%)";
-    document.getElementById("new-website").style.transform = "translateX(0)";
-  }, 7000);
+setTimeout(function(){
+  document.querySelector('.old-website').classList.add('hidden');
+  document.querySelector('.new-website').classList.remove('hidden');
+}, 7000); // wait 7 seconds before transitioning to the new website
 </script>
