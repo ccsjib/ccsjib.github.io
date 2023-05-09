cb4c577a5e75417ccf387dadbf4f35532ef60077
ccsjib <jsyed@cannonschool.org>
2023-05-09 14:34:08 -0400

    new script js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-setTimeout(
-  function(){
-    document.querySelector('.old-website').classList.add('hidden');
-    document.querySelector('.new-website').classList.remove('hidden');
-  }
-  , 7000); 
-
-
-// wait 7 seconds before transitioning to the new website
+setTimeout(function() {
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("old-website").style.display = "none";
+  document.getElementById("new-website").style.display = "block";
+}, 7000);
