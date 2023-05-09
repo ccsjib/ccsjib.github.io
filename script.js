a649aa214569023eaefbaa496841971c86ebb541
ccsjib <jsyed@cannonschool.org>
2023-05-09 14:28:17 -0400

    7 seconds

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ setTimeout(
     document.querySelector('.old-website').classList.add('hidden');
     document.querySelector('.new-website').classList.remove('hidden');
   }
-  , 1000); 
+  , 7000); 
 
 
 // wait 7 seconds before transitioning to the new website
