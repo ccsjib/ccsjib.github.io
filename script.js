f771862660e3066b134c6746afed29f1aa86fbf5
ccsjib <jsyed@cannonschool.org>
2023-05-09 14:07:07 -0400

    remove script tag

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-<script>
-setTimeout(function(){
-  document.querySelector('.old-website').classList.add('hidden');
-  document.querySelector('.new-website').classList.remove('hidden');
-}, 7000); // wait 7 seconds before transitioning to the new website
-</script>
+setTimeout(
+  function(){
+    document.querySelector('.old-website').classList.add('hidden');
+    document.querySelector('.new-website').classList.remove('hidden');
+  }
+  , 7000); 
+
+
+// wait 7 seconds before transitioning to the new website
