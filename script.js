98964b35cfbdafe73e2ddb25a310d8c33368e6d5
ccsjib <jsyed@cannonschool.org>
2023-05-09 14:54:06 -0400

    coimmit

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 window.onload = function() {
-  setTimeout(function(){
-    var old_website = document.getElementById("old-website");
-    var new_website = document.getElementById("new-website");
-    var popup = document.getElementById("popup");
-    
-    old_website.style.display = "none";
-    new_website.style.display = "block";
-    
-    popup.style.display = "block";
-    
-    setTimeout(function(){
-      popup.style.display = "none";
+  setTimeout(function() {
+    document.getElementById('old-website').style.display = 'none';
+    document.getElementById('new-website').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
+    setTimeout(function() {
+      document.getElementById('popup').style.display = 'none';
     }, 5000);
   }, 7000);
 };
