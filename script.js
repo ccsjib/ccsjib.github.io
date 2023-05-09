f883bffd2726ba1eaae2de6c5328bb103153c4a0
ccsjib <jsyed@cannonschool.org>
2023-05-09 14:38:40 -0400

    more js fixing

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-setTimeout(function() {
-  document.getElementById("popup").style.display = "block";
-  document.getElementById("old-website").style.display = "none";
-  document.getElementById("new-website").style.display = "block";
-}, 7000);
+window.onload = function() {
+  setTimeout(function(){
+    var old_website = document.getElementById("old-website");
+    var new_website = document.getElementById("new-website");
+    var popup = document.getElementById("popup");
+    
+    old_website.style.display = "none";
+    new_website.style.display = "block";
+    
+    popup.style.display = "block";
+    
+    setTimeout(function(){
+      popup.style.display = "none";
+    }, 5000);
+  }, 7000);
+};
